fac50aec67a57a0b8a0b9dbbc8e620397a11b187
trevor-scofield <20018573+trevor-scofield@users.noreply.github.com>
2023-10-02 12:41:12 -0400

    Update gettingStarted.jsx
    putting in the Header.jsx and Header.css files needed for file problem 3

--- gettingStarted.jsx
+++ gettingStarted.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
+import Header from '/components/header/Header.css';
+import Header from '../components/header/Header.jsx';
 
 import Example from './components/example/Example';
 
