b9c2a917ab18b3e702af1a7286c8f575c3693411
santi <143372211+santip5@users.noreply.github.com>
2023-10-02 16:43:22 -0400

    Completed problem 5

--- p5.jsx
+++ p5.jsx
@@ -1,4 +1,43 @@
-//p5 jsx file
+//p4 jsx file
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Link } from "react-router-dom";
+import './styles/main.css'
+import Header from '/components/header/Header';
+import States from './components/states/States';
+import Example from './components/example/Example';
 
-import Header from '../components/header/Header.jsx';
-import Header from '../components/header/Header.css';
+
+
+
+
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/example">Example</Link>
+                        </li>
+                        <li>
+                            <Link to="/states">States</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <hr />
+                <Route path="/example" component={Example} />
+                <Route path="/states" component={States} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <HashRouter>
+        <Main />
+    </HashRouter>,
+    document.getElementById('reactapp')
+);
