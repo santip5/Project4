80c9d434d17df18c15c48b009d32fdf7acf56e10
trevor-scofield <20018573+trevor-scofield@users.noreply.github.com>
2023-10-02 12:49:57 -0400

    Update p2.jsx
    adding the imports for the Header

--- p2.jsx
+++ p2.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from '/components/header/Header.css';
+import Header from '../components/header/Header.jsx';
 
 import States from './components/states/States';
 
